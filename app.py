68747a2de13eba0f3f4dfdfe38ca1c164ed225ba
Zhanyyyl <166705797+Zhanyyyl@users.noreply.github.com>
2025-10-09 18:48:35 +0500

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return '<h1>Менің Azure қосымшам жұмыс істейді!</h1>'
-
-if __name__ == '__main__':
-    app.run()
